d66e5e92f25bbaf36966d202a857b4c26d8e65ea
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-02-09 09:14:50 +0000

    web interface for NeDB

---
+++ app.js
@@ -0,0 +1,74 @@
+const nedb = require("nedb");
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "./public")));
+
+const db = new nedb({ filename: "emp.db", autoload: true });
+console.log("db created");
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+//add
+app.post("/add", function (req, res) {
+  db.insert({ name: req.body.name }, function (err, newDoc) {
+    if (err) {
+      console.log("error", err);
+    } else {
+      console.log("document inserted", newDoc);
+    }
+  });
+});
+// View
+app.post("/view", function (req, res) {
+  db.find({ _id: req.body.id }, function (err, docs) {
+    if (err) {
+      console.log("error");
+    } else {
+      console.log("documents retrieved: ", docs);
+    }
+  });
+});
+//UPDATE
+app.post("/update", function (req, res) {
+  db.update(
+    { _id: req.body._id  },
+    { $set: { name: req.body.name  } },
+    {},
+    function (err, docs) {
+      if (err) {
+        console.log("error updating documents", err);
+      } else {
+        console.log(docs, "documents updated");
+      }
+    }
+  );
+});
+//SHOW ALL
+app.post("/showAll", function (req, res) {
+  db.find({}, {}, function (err, docs) {
+    if (err) {
+      console.log("error deleting document");
+    } else {
+      console.log(docs);
+    }
+  });
+});
+//DELETE
+app.post("/delete", function (req, res) {
+  db.remove({ _id: req.body.id  }, {}, function (err, docsRem) {
+    if (err) {
+      console.log("error deleting document");
+    } else {
+      console.log(docsRem, "document removed from database");
+    }
+  });
+});
+server.listen(3000, function () {
+  console.log("Server listening on port: 3000");
+});
