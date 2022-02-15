cf3d0df9c965122c7c13031874b4dc9cbe32a66d
FionaMacRaeFairlie <fionamacraefairlie@gmail.com>
2022-02-15 10:59:19 +0000

    Lab 4a

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const nedb = require("nedb");
 const express = require("express");
 const path = require("path");
-const app = express();
 
+const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
 
