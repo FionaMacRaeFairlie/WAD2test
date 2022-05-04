e3175b7037a8819afa3bd018bd1f458be37b7d60
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-05-04 12:41:34 +0100

    test

--- index.js
+++ index.js
@@ -9,65 +9,46 @@ app.use(express.static(path.join(__dirname, "./public")));
 const db = new nedb({ filename: "emp.db", autoload: true });
 console.log("db created");
 
+const mustache = require('mustache-express');
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-//add
-app.post("/add", function (req, res) {
-  db.insert({ name: req.body.name }, function (err, newDoc) {
-    if (err) {
-      console.log("error", err);
-    } else {
-      console.log("document inserted", newDoc);
-    }
-  });
-});
-// View
-app.post("/view", function (req, res) {
-  db.find({ _id: req.body.id }, function (err, docs) {
-    if (err) {
-      console.log("error");
-    } else {
-      console.log("documents retrieved: ", docs);
-    }
-  });
-});
-//UPDATE
-app.post("/update", function (req, res) {
-  db.update(
-    { _id: req.body._id  },
-    { $set: { name: req.body.name  } },
-    {},
-    function (err, docs) {
-      if (err) {
-        console.log("error updating documents", err);
-      } else {
-        console.log(docs, "documents updated");
-      }
-    }
-  );
-});
-//SHOW ALL
-app.post("/showAll", function (req, res) {
-  db.find({}, {}, function (err, docs) {
-    if (err) {
-      console.log("error deleting document");
-    } else {
-      console.log(docs);
-    }
-  });
-});
 //DELETE
-app.post("/delete", function (req, res) {
-  db.remove({ _id: req.body.id  }, {}, function (err, docsRem) {
+/*delete_food =function(req,res){
+  console.log('deleting food');
+  if(!req.body.name){
+    res.status(400).send("Insert food name");
+    return;
+  }
+db.deleteEntry(req.body.name);
+res.redirect("/");
+}*/
+exports.delete_food= function (req, res) {
+  db.remove({ name: req.body.name }, {}, function (err, docsRem) {
     if (err) {
       console.log("error deleting document");
     } else {
       console.log(docsRem, "document removed from database");
+      res.redirect("/");
     }
-  });
-});
+  })
+}
+app.post("/delete",delete_food);
 
 app.listen(3000,() =>{
   console.log("Server listening on port: 3000");
 });
+
+
+new_delete_food =function(req,res){
+  res.render('deleteFood', {
+    'title':'Delete Food'
+  })
+}
+  
+
+
